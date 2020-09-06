42e1dc306794efab237b6d3c5bcc0b2a26db3798
Brahm karan singh <brahmkaran123@gmail.com>
2020-09-06 13:42:07 +0530

    Update SnakeGunWater.py

--- SnakeGunWater.py
+++ SnakeGunWater.py
@@ -1,36 +1,36 @@
 import random
 l=["Snake","Water","Gun"]
-comp=0;mard=0
+comp=0;you=0
 i=0
 while(i<5):
-    man=input('Enter your choice-"S for snake" "G for gun" "W for water"').lower()
+    person=input('Enter your choice-"S for snake" "G for gun" "W for water"').lower()
     computer=random.choice(l)
     print('Your choice is',man,'and computers choice is',computer )
-    if man=='s' or man=='w' or man=='g':
+    if person=='s' or person=='w' or person=='g':
         if man!=computer:
-            if man=='s' and computer=='Water':
-                mard+=1
-            elif man=='s' and computer=='Gun':
-                comp+=1
-            elif man=='g' and computer=='Snake':
-                mard+=1
-            elif man=='g' and computer=='Water':
-                comp+=1
-            elif man=='w' and computer=='Snake':
-                comp+=1
-            elif man=='w' and computer=='Gun':
-                mard+=1
+            if person=='s' and computer=='Water':
+                you+=1
+            elif person=='s' and computer=='Gun':
+                you+=1
+            elif person=='g' and computer=='Snake':
+                you+=1
+            elif person=='g' and computer=='Water':
+                you+=1
+            elif person=='w' and computer=='Snake':
+                you+=1
+            elif person=='w' and computer=='Gun':
+                you+=1
         else:
-            mard=mard;comp=comp
+            you=you;comp=comp
 
     else:
         print('Please check if your input is correct or not')
         i-=1
     i+=1
-if mard>comp:
-    print("Winner is man","Scores are- Man=",mard,"Computer=",comp)
-elif mard<comp:
-    print("Winner is computer", "Scores are- Man=", mard, "Computer=", comp)
+if you>comp:
+    print("Winner is man","Scores are- Person=",you,"Computer=",comp)
+elif you<comp:
+    print("Winner is computer", "Scores are- Person=", you, "Computer=", comp)
 else:
     print("It is a draw")
 
